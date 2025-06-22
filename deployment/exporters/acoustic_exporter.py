39e4921e02fe716766bae021d7b1adfb81d03aef
Ghin_MLo7 <98134320+MLo7Ghinsan@users.noreply.github.com>
2025-06-22 02:53:27 -0500

    commenting out optimizer

--- deployment/exporters/acoustic_exporter.py
+++ deployment/exporters/acoustic_exporter.py
@@ -362,25 +362,25 @@ class DiffSingerAcousticExporter(BaseExporter):
         onnx_helper.model_override_io_shapes(diffusion, output_shapes={
             'mel': (1, 'n_frames', hparams['audio_num_mel_bins'])
         })
-        print(f'Running ONNX Simplifier #1 on {self.diffusion_class_name}...')
-        diffusion, check = onnxsim.simplify(diffusion, include_subgraph=True)
-        assert check, 'Simplified ONNX model could not be validated'
+        #print(f'Running ONNX Simplifier #1 on {self.diffusion_class_name}...')
+        #diffusion, check = onnxsim.simplify(diffusion, include_subgraph=True)
+        #assert check, 'Simplified ONNX model could not be validated'
         onnx_helper.graph_fold_back_to_squeeze(diffusion.graph)
         weight_pattern = r'diffusion\..*\.conditioner_projection\.weight'
         if self.backbone_class_name == 'DiffusionTransformer':
             weight_pattern = r'diffusion\..*\.(?:conditioner_projection|cond_proj)\.weight'
-        onnx_helper.graph_extract_conditioner_projections(
-            graph=diffusion.graph, op_type='Conv',
-            weight_pattern=weight_pattern,
-            alias_prefix='/diffusion/backbone/cache'
-        )
+        #onnx_helper.graph_extract_conditioner_projections(
+        #    graph=diffusion.graph, op_type='Conv',
+        #    weight_pattern=weight_pattern,
+        #    alias_prefix='/diffusion/backbone/cache'
+        #)
         onnx_helper.graph_remove_unused_values(diffusion.graph)
-        print(f'Running ONNX Simplifier #2 on {self.diffusion_class_name}...')
-        diffusion, check = onnxsim.simplify(
-            diffusion,
-            include_subgraph=True
-        )
-        assert check, 'Simplified ONNX model could not be validated'
+        #print(f'Running ONNX Simplifier #2 on {self.diffusion_class_name}...')
+        #diffusion, check = onnxsim.simplify(
+        #    diffusion,
+        #    include_subgraph=True
+        #)
+        #assert check, 'Simplified ONNX model could not be validated'
         print(f'| optimize graph: {self.diffusion_class_name}')
         return diffusion
 
